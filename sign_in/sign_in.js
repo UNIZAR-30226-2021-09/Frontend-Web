550aa7856af5431e7769bd2b402b97e69855a7a7
Adrian-uni <79143545+Adrian-uni@users.noreply.github.com>
2021-03-22 18:37:32 +0100

    Add files via upload
    no se si crear un nuevo branch por si debemos corregir cosas, por si acaso lo dejo en la opción por defecto

---
+++ sign_in/sign_in.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+
+    el: '#BatallaNaval_App',
+    data: {
+        name = "",
+        mail = "",
+        passwd = "",
+        rep_passwd = ""
+    },
+    methods:{
+
+        registrarse(){
+            //si el nombre de usuario esta disponible
+                //si las contraseñas son iguales
+                    //introducir los datos en la base de datos
+                //sino informamos del error recargando la pagina
+            //sino informamos del error recargando la pagina
+        }
+    }
+})
