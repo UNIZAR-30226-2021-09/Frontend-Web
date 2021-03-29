4d1d0638db4e71daecf76ac82b7de0f9b0e1fd2b
Adrian-uni <79143545+Adrian-uni@users.noreply.github.com>
2021-03-29 11:49:07 +0200

    Actualizacion sign in

--- src/autenticacion/sign_in.js
+++ src/autenticacion/sign_in.js
@@ -1,20 +1,34 @@
 const app = new Vue({
+    el: '#sign_in',
+    data: {
+        titulo: 'Sign in',
+        correo: '',
+        contrasena: '',
+        rep_contrasena: ''
 
-  el: '#BatallaNaval_App',
-  data: {
-      name = "",
-      mail = "",
-      passwd = "",
-      rep_passwd = ""
-  },
-  methods:{
+    },
 
-      registrarse(){
-          //si el nombre de usuario esta disponible
-              //si las contraseñas son iguales
-                  //introducir los datos en la base de datos
-              //sino informamos del error recargando la pagina
-          //sino informamos del error recargando la pagina
-      }
-  }
+    methods:{
+        registrarse(){
+            //si el nombre de usuario esta disponible
+                //si las contraseñas son iguales
+                    //introducir los datos en la base de datos
+                //sino informamos del error recargando la pagina
+            //sino informamos del error recargando la pagina
+        }
+
+
+    }
+
 })
+
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      chr   = this.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
