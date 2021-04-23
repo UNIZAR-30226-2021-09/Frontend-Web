422316e6761cabf1e8fa4c43f17e099c52651233
Santiago Jiménez <62775651+Santy172000@users.noreply.github.com>
2021-04-23 21:07:45 +0200

    Iconos bootstrap
    Faltan las fotos del inicio

--- src/components/ListaAmigos.vue
+++ src/components/ListaAmigos.vue
@@ -7,7 +7,15 @@
     <div class="col-sm-3">
         
         <h4 class="text-sm-left">{{perfil.nombreUsuario}} 
-            <router-link to="perfil" class="btn btn-primary btn-sm mb-1" type="button" >Mi Perfil</router-link>
+            <div class="btn-group">
+                <router-link to="perfil" class="btn btn-primary btn-sm mb-1" type="button" >Mi Perfil</router-link>
+                <router-link to="perfil" class="btn btn-primary btn-sm mb-1" type="button" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+                        <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"/>
+                    </svg>
+                </router-link>
+            </div>
+            
         </h4>
 
         <div class="mt-3">
@@ -28,8 +36,16 @@
         <br>
 
         <div>
-            <h5 style="display:inline">Amigos </h5>  <router-link to="agregarAmigo" class="btn btn-outline-success btn-sm mb-1" type="button">+</router-link>
+            <h5 style="display:inline">Amigos </h5>  
+            <router-link to="agregarAmigo" class="btn btn-outline-success btn-sm mb-1" type="button">
+                <!-- Botón de añadir amigo -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
+                    <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                    <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+                </svg>
+            </router-link>
         </div>
+        <p></p>
 
         <i v-if="numAmigos==0">Aún no has añadido a ningún amigo</i>
 
