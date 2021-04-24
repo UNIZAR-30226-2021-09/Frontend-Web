dfb4e1287cefcf09817dca3c6bedb2a782c8e299
Adrian-uni <79143545+Adrian-uni@users.noreply.github.com>
2021-04-24 13:39:55 +0200

    partidas en curso primera version

--- src/views/Partida.vue
+++ src/views/Partida.vue
@@ -91,14 +91,14 @@ export default {
     }
 
     //Create Board
-    function createBoard(grid, squares) {
+    /*function createBoard(grid, squares) {
       for (let i = 0; i < width*width; i++) {
         const square = document.createElement('div')
         square.dataset.id = i
         grid.appendChild(square)
         squares.push(square)
       }
-    }
+    }*/
 
     
   }
--- src/views/PartidasEnCurso.vue
+++ src/views/PartidasEnCurso.vue
@@ -9,6 +9,26 @@
 
                 <h2>{{nombrePag}}</h2>
 
+                <br> <br> <br>
+                <div v-for="(index,partida) in partidasEnCurso" v-bind:key="index">
+
+                    <div v-if="partida.turno === this.perfil.nombreUsuario">
+                      <li class="list-group-item list-group-item-success">
+                        Partida contra {{partida.rival}} 
+                        <router-link to="partida" class="btn btn-light btn-sm mb-1 mt-1" type="button" >Continuar la partida</router-link>
+                      </li>
+                    </div>
+
+                    <div v-else>
+                      <li class="list-group-item list-group-item-warning">
+                        Partida contra {{partida.rival}}, esperando a movimiento del contrincante
+                      </li>
+                    </div>
+
+                </div>
+
+                <i v-if="numPartidas==0">Actualmente no tienes partidas en curso.</i>
+
             </div>
             <ListaAmigos/>
          </div>
@@ -21,6 +41,8 @@
 #######################################SCRIPT#######################################
 <script>
 import ListaAmigos from '@/components/ListaAmigos.vue'
+import {mapState} from 'vuex';
+import axios from 'axios'
 export default {
   name: 'PartidasEnCurso',
   components: {
@@ -28,11 +50,35 @@ export default {
   },
   data() {
         return{ 
-          nombrePag: 'Partidas en curso'
+          nombrePag: 'Partidas en curso',
+          numPartidas: 0
         }
   },
   methods: {
 
+  },
+  asyncComputed: {
+      ...mapState(['perfil']), //Para recoger los datos de la lista de amigos que están almacenados en el store
+      partidasEnCurso(){
+
+          let dir = this.host + '/game/inProgress'  
+          axios
+          .post(dir, {
+              nombreUsuario: this.perfil.nombreUsuario,
+              accessToken: this.perfil.accessToken
+          })
+          .then(resp => {
+              this.setPartidas(resp.data);
+              this.numPartidas = this.parfil.partidasEnCurso.lenth;
+              return this.perfil.partidasEnCurso;
+            })
+
+          .catch(error => {
+            //Error al hacer login
+            console.log(error.response.request.response)
+            });
+          return [];
+      },
   }
 }
 
