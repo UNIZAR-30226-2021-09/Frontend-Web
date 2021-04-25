af15750ca3f32c1020f178b43f6cc22ee8c6b842
Santiago Jiménez <62775651+Santy172000@users.noreply.github.com>
2021-04-25 12:29:08 +0200

    Añadir gestión de los barcos
    Hay algún tipo de problema cuando cojo algunos barcos (los que son más grandes) estando en vertical, ya que cuando imprimo qué tipo de barco estoy cogiendo me saca un string vacío, por lo que entiendo que en algún punto se perderá la referencia.

--- src/views/Partida.vue
+++ src/views/Partida.vue
@@ -18,7 +18,7 @@
                 <div class="container hidden-info">
                   <div class="setup-buttons" id="setup-buttons">
                     <button id="start" class="btn">Start Game</button>
-                    <button id="rotate" class="btn">Rotate Your Ships</button>
+                    <button id="rotate" class="btn" >Rotate Your Ships</button>
                   </div>
                   <h3 id="whose-go" class="info-text">Your Go</h3>
                   <h3 id="info" class="info-text"></h3>
@@ -57,10 +57,6 @@ export default {
   data() {
         return{ 
           titulo: 'Partida',
-          nombre: '',
-          link: '',
-          esEnviado: false,
-          datosInvalidos: false
         }
   },
   methods: {
@@ -68,41 +64,218 @@ export default {
   },
   mounted() {
     const userGrid = document.querySelector('.grid-user')
+    const computerGrid = document.querySelector('.grid-computer')
     const gridIdentifier = userGrid.attributes.item(0).name
+    const compGridIdentifier = computerGrid.attributes.item(0).name
     // console.log('User grid:')
     // console.log(userGrid)
     // console.log('El identificador del grid del usuario es ' + gridIdentifier)
 
-    // const computerGrid = document.querySelector('.grid-computer')
-    // const displayGrid = document.querySelector('.grid-display')
-    // const ships = document.querySelectorAll('.ship')
-    // const destroyer = document.querySelector('.destroyer-container')
-    // const submarine = document.querySelector('.submarine-container')
-    // const cruiser = document.querySelector('.cruiser-container')
-    // const battleship = document.querySelector('.battleship-container')
-    // const carrier = document.querySelector('.carrier-container')
+    const displayGrid = document.querySelector('.grid-display')
+    const ships = document.querySelectorAll('.ship')
+    const destroyer = document.querySelector('.destroyer-container')
+    const submarine = document.querySelector('.submarine-container')
+    const cruiser = document.querySelector('.cruiser-container')
+    const battleship = document.querySelector('.battleship-container')
+    const carrier = document.querySelector('.carrier-container')
+    // const startButton = document.querySelector('#start')
+    const rotateButton = document.querySelector('#rotate')
+    // const turnDisplay = document.querySelector('#whose-go')
+    // const infoDisplay = document.querySelector('#info')
+    // const setupButtons = document.getElementById('setup-buttons')
         
     let userSquares = []
+    let computerSquares = []
+    let isHorizontal = true
+    // let allShipsPlaced = false
+
+    //Array de los barcos con sus posibles orientaciones (horizontal y vertical)
+    // const shipArray = [
+    //   {
+    //     name: 'destroyer',
+    //     directions: [
+    //       [0, 1],
+    //       [0, width]
+    //     ]
+    //   },
+    //   {
+    //     name: 'submarine',
+    //     directions: [
+    //       [0, 1, 2],
+    //       [0, width, width*2]
+    //     ]
+    //   },
+    //   {
+    //     name: 'cruiser',
+    //     directions: [
+    //       [0, 1, 2],
+    //       [0, width, width*2]
+    //     ]
+    //   },
+    //   {
+    //     name: 'battleship',
+    //     directions: [
+    //       [0, 1, 2, 3],
+    //       [0, width, width*2, width*3]
+    //     ]
+    //   },
+    //   {
+    //     name: 'carrier',
+    //     directions: [
+    //       [0, 1, 2, 3, 4],
+    //       [0, width, width*2, width*3, width*4]
+    //     ]
+    //   },
+    // ]
+
     const width = 10
 
-    createBoard(userGrid, userSquares)
-    
     //Create Board
-    function createBoard(grid, squares) {
+    function createBoard(grid, squares, id) {
       for (let i = 0; i < width*width; i++) {
         const square = document.createElement('div')
         square.dataset.id = i
         //Hay que meterle el identificador interno que le pone vue al padre, porque sino no coge los estilos bien
-        var t = document.createAttribute(gridIdentifier) 
+        var t = document.createAttribute(id) 
         square.attributes.setNamedItem(t)
         //
         grid.appendChild(square)
         squares.push(square)
       }
     }
-
     
+    createBoard(userGrid, userSquares, gridIdentifier)
+    createBoard(computerGrid, computerSquares, compGridIdentifier)
+
+    //Función para rotar los barcos
+    function rotate() {
+      destroyer.classList.toggle('destroyer-container-vertical')
+      submarine.classList.toggle('submarine-container-vertical')
+      cruiser.classList.toggle('cruiser-container-vertical')
+      battleship.classList.toggle('battleship-container-vertical')
+      carrier.classList.toggle('carrier-container-vertical')
+      if (isHorizontal) {
+        isHorizontal = false
+        return
+      }else { //Está en vertical
+        isHorizontal = true
+        return
+      }
+    }
+
+    rotateButton.addEventListener('click', rotate)
+
+    //A cada barco le asignamos la función dragStart si ocurre el evento de drag
+    ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
+    //A los cuadraditos les asignamos todos los eventos necesarios
+    userSquares.forEach(square => square.addEventListener('dragstart', dragStart))
+    userSquares.forEach(square => square.addEventListener('dragover', dragOver))
+    userSquares.forEach(square => square.addEventListener('dragenter', dragEnter))
+    userSquares.forEach(square => square.addEventListener('dragleave', dragLeave))
+    userSquares.forEach(square => square.addEventListener('drop', dragDrop))
+    userSquares.forEach(square => square.addEventListener('dragend', dragEnd))
+
+    //Variables que nos servirán para guardar información relevante
+    let selectedShipNameWithIndex
+    let draggedShip
+    let draggedShipLength
+
+    //Para obtener los ids de los barcos que estamos cogiendo
+    ships.forEach(ship => ship.addEventListener('mousedown', (e) => {
+      selectedShipNameWithIndex = e.target.id
+    }))
+
+    function dragStart() {
+      //Nos guardamos la información de qué barco estamos cogiendo y qué longitud tiene
+      draggedShip = this
+      draggedShipLength = this.childNodes.length
+      // console.log(draggedShip)
+    }
+
+    function dragOver(e) {
+      e.preventDefault()
+    }
+
+    function dragEnter(e) {
+      e.preventDefault()
+    }
+
+    function dragLeave() {
+      // console.log('drag leave')
+    }
+
+    function isEmpty(isH, dragLen, selectedShipIndex, id) {
+      let seems = true
+      if(isH){
+        for (let i=0; i < dragLen; i++) {
+          let element = userSquares[id - selectedShipIndex + i]
+          if ((' ' + element.className + ' ').indexOf('taken') > -1) seems = false
+        }
+      }else{
+        for (let i=0; i < dragLen; i++) {
+          let element = userSquares[id - selectedShipIndex * i]
+          if ((' ' + element.className + ' ').indexOf('taken') > -1) seems = false
+        }
+      }
+      return seems
+
+    }
+
+    function dragDrop() {
+      let shipNameWithLastId = draggedShip.lastChild.id
+      let shipClass = shipNameWithLastId.slice(0, -2)
+      console.log('Barco: ' + shipClass)
+      let lastShipIndex = parseInt(shipNameWithLastId.substr(-1))
+      let shipLastId = lastShipIndex + parseInt(this.dataset.id)
+      // console.log(shipLastId)
+      const notAllowedHorizontal = [0,10,20,30,40,50,60,70,80,90,1,11,21,31,41,51,61,71,81,91,2,22,32,42,52,62,72,82,92,3,13,23,33,43,53,63,73,83,93]
+      const notAllowedVertical = [99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60]
+      
+      let newNotAllowedHorizontal = notAllowedHorizontal.splice(0, 10 * lastShipIndex)
+      let newNotAllowedVertical = notAllowedVertical.splice(0, 10 * lastShipIndex)
+
+      console.log('Cojo ' + selectedShipNameWithIndex)
+
+      let selectedShipIndex = parseInt(selectedShipNameWithIndex.substr(-1))
+
+      shipLastId = shipLastId - selectedShipIndex
+      console.log('Acaba: ' + shipLastId)
+
+      let empty = isEmpty(isHorizontal, draggedShipLength, selectedShipIndex, parseInt(this.dataset.id))
+
+      console.log('Empty = ' + empty)
+
+      if (isHorizontal && !newNotAllowedHorizontal.includes(shipLastId) && empty) {
+        for (let i=0; i < draggedShipLength; i++) {
+          // // let directionClass
+          // // if (i === 0) directionClass = 'start'
+          // // if (i === draggedShipLength - 1) directionClass = 'end'
+          //Pintamos el barco en el tablero
+          userSquares[parseInt(this.dataset.id) - selectedShipIndex + i].classList.add('taken', shipClass)
+        }
+      //As long as the index of the ship you are dragging is not in the newNotAllowedVertical array! This means that sometimes if you drag the ship by its
+      //index-1 , index-2 and so on, the ship will rebound back to the displayGrid.
+      } else if (!isHorizontal && !newNotAllowedVertical.includes(shipLastId) && empty) {
+        for (let i=0; i < draggedShipLength; i++) {
+          // let directionClass
+          // if (i === 0) directionClass = 'start'
+          // if (i === draggedShipLength - 1) directionClass = 'end'
+          userSquares[parseInt(this.dataset.id) - selectedShipIndex + width*i].classList.add('taken', shipClass)
+        }
+      } else return
+      // console.log(userSquares)
+
+      displayGrid.removeChild(draggedShip) //Quitamos el barco de abajo
+      // if(!displayGrid.querySelector('.ship')) allShipsPlaced = true
+    }
+
+    function dragEnd() {
+      // console.log('dragend')
+    }
+
+
   }
+
 }
 
 
@@ -124,15 +297,15 @@ export default {
     height: 400px;
     display: flex; /* Esto sirve para que los cuadraditos estén juntos */
     flex-wrap: wrap;
-    background-color: blue;
+    background-color: lightblue;
     margin: 20px;
   }
 
   .battleship-grid div { /*Cualquier grid que esté en un div tendrá este estilo */
     width: 40px;
     height: 40px;
-    outline: 0.5px solid blue;
-    background-color: lightblue;
+    outline: 0.5px solid black;
+    /* background-color: lightblue; */
   }
 
   .grid-computer {
@@ -159,12 +332,37 @@ export default {
     display: flex;
   }
 
+  .destroyer-container-vertical {
+    width: 40px;
+    height: 80px;
+    background-color: orange;
+    margin: 10px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  
+  .destroyer {
+    background-color: orange;
+  }
+
   .submarine-container {
     width: 120px;
     height: 40px;
     background-color: pink;
     margin: 10px;
     display: flex;
+  }
+
+  .submarine-container-vertical {
+    width: 40px;
+    height: 120px;
+    background-color: pink;
+    margin: 10px;
+    display: flex;
+  }
+
+  .submarine{
+    background-color: pink;
   }
 
   .cruiser-container {
@@ -175,12 +373,36 @@ export default {
     display: flex;
   }
 
+  .cruiser-container-vertical {
+    width: 40px;
+    height: 120px;
+    background-color: purple;
+    margin: 10px;
+    display: flex;
+  }
+
+  .cruiser {
+    background-color: purple;
+  }
+
   .battleship-container {
     width: 160px;
     height: 40px;
     background-color: aqua;
     margin: 10px;
     display: flex;
+  }
+
+  .battleship-container-vertical {
+    width: 40px;
+    height: 160px;
+    background-color: aqua;
+    margin: 10px;
+    display: flex;
+  }
+
+  .battleship {
+    background-color: aqua;
   }
 
   .carrier-container {
@@ -191,11 +413,24 @@ export default {
     display: flex;
   }
 
+  .carrier-container-vertical {
+    width: 40px;
+    height: 200px;
+    background-color: green;
+    margin: 10px;
+    display: flex;
+  }
+
+  .carrier {
+    background-color: green;
+  }
+
   .ship div{
     width: 40px;
     height: 40px;
     cursor: grab;
   }
+
 
 
 </style>
