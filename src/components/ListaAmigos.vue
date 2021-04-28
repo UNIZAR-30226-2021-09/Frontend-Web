21ee4940cfdad054b21ba87e74031002306167ff
Adrian-uni <79143545+Adrian-uni@users.noreply.github.com>
2021-04-29 01:57:24 +0200

    scroll en lista de amigos

--- src/components/ListaAmigos.vue
+++ src/components/ListaAmigos.vue
@@ -66,17 +66,20 @@
 
         <!-- Lista de amigos conectados -->
         <ul class="mt-2">
-            <div v-for="amigo in amigos" v-bind:key="amigo.nombre" >
-                <a class="container btn border border-3" href="/Perfil" role="button" @click="cambiarBuscado(amigo.nombre)">
-                   <p style="margin: 7px;"> 
-                        <svg style="margin-right: 10px;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-file-person" viewBox="0 0 16 16">
-                            <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
-                            <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                        </svg> {{amigo.nombre}}
-                   </p>
-                </a>
-                <p style="background-color: coral;">-</p>
+            <div class="overflow-auto p-3 mb-3 mb-md-0 mr-md-3 bg-light" style="max-width: 260px; max-height: 400px;">
+                <div v-for="amigo in amigos" v-bind:key="amigo.nombre" >
+                    <a class="container btn border border-3" href="/Perfil" role="button" @click="cambiarBuscado(amigo.nombre)">
+                    <p style="margin: 7px;"> 
+                            <svg style="margin-right: 10px;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-file-person" viewBox="0 0 16 16">
+                                <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
+                                <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                            </svg> {{amigo.nombre}}
+                    </p>
+                    </a>
+                    <p style="background-color: coral;">-</p>
+                </div>
             </div>
+
         </ul>
 
         <br>
