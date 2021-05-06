b0b9bf2597cd388a82a25daecb77107c2bd4558d
Santiago Jiménez <62775651+Santy172000@users.noreply.github.com>
2021-05-06 17:17:38 +0200

    Iconitos

--- src/views/Partida.vue
+++ src/views/Partida.vue
@@ -10,8 +10,19 @@
               <!-- Contenido de la pantalla -->
                 <!-- <input type="button" value="Click Me" style="float: right;"> -->
                 <div class="btn-group" style="float: right;">
-                  <a href="inicio" class="btn btn-warning">Salir</a>
-                  <a href="inicio" class="btn btn-warning">Rendirme</a>
+                  <a href="inicio" class="btn btn-warning">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="currentColor" class="bi bi-flag" viewBox="0 0 16 18">
+                      <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001M14 1.221c-.22.078-.48.167-.766.255-.81.252-1.872.523-2.734.523-.886 0-1.592-.286-2.203-.534l-.008-.003C7.662 1.21 7.139 1 6.5 1c-.669 0-1.606.229-2.415.478A21.294 21.294 0 0 0 3 1.845v6.433c.22-.078.48-.167.766-.255C4.576 7.77 5.638 7.5 6.5 7.5c.847 0 1.548.28 2.158.525l.028.01C9.32 8.29 9.86 8.5 10.5 8.5c.668 0 1.606-.229 2.415-.478A21.317 21.317 0 0 0 14 7.655V1.222z"/>
+                    </svg>
+                    Rendirme
+                  </a>
+                  <a href="inicio" class="btn btn-warning">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 18">
+                      <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                      <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                    </svg>
+                    Salir 
+                  </a>
                 </div>
 
                 <p></p>
@@ -32,9 +43,25 @@
 
                 <div class="container hidden-info">
                   <div class="setup-buttons btn-group" id="setup-buttons">
-                    <button id="start" class="btn btn-outline-primary">Empezar partida</button>
-                    <button id="rotate" class="btn btn-outline-primary" >Rotar los barcos</button>
-                    <button id="restart" class="btn btn-outline-primary" >Reiniciar mi tablero</button>
+                    <button id="start" class="btn btn-outline-primary">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+                      </svg>
+                      Empezar partida
+                    </button>
+                    <button id="rotate" class="btn btn-outline-primary" >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 18">
+                        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                      </svg>
+                      Rotar los barcos
+                    </button>
+                    <button id="restart" class="btn btn-outline-primary" >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="currentColor" class="bi bi-eraser" viewBox="0 0 16 18">
+                        <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm2.121.707a1 1 0 0 0-1.414 0L4.16 7.547l5.293 5.293 4.633-4.633a1 1 0 0 0 0-1.414l-3.879-3.879zM8.746 13.547 3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z"/>
+                      </svg>
+                      Reiniciar mi tablero
+                    </button>
                   </div>
                   <h3 id="whose-go" class="info-text">Your Go</h3>
                   <h3 id="info" class="info-text"></h3>
@@ -231,7 +258,7 @@ export default {
       //userSquares.forEach(element => console.log(element.classList)) //Imprimimos las clases de cada uno de los recuadros
       
       //Limpiamos el tablero
-      displayGrid.children.forEach(elem => displayGrid.removeChild(document.querySelector(('#'+elem.id))))
+      displayGrid.children.forEach(elem => displayGrid.removeChild(document.querySelector('#'+elem.id)))
       
       // displayGrid.removeChild(document.querySelector('#destroyer'))
       // displayGrid.removeChild(document.querySelector('#submarine'))
