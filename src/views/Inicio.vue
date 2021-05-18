8071d31fbd614aaeddc7b218be5f262b78ea824a
Santiago Jiménez <62775651+Santy172000@users.noreply.github.com>
2021-05-18 12:27:16 +0200

    Foto de partidas en curso

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -97,7 +97,7 @@
                       </div>
                     </div>
                     <div class="carousel-item h-50" >
-                      <router-link to="partidasEnCurso"><img src="@/assets/logo.png" width="750" height="550" class="d-block" alt="@/assets/logo.png" ></router-link>
+                      <router-link to="partidasEnCurso"><img src="@/assets/partidas_curso.jpg" width="750" height="550" class="d-block" alt="@/assets/logo.png" ></router-link>
                       <div class="carousel-caption d-none d-md-block bg-white rounded-pill">
                         <h4>
                           <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
