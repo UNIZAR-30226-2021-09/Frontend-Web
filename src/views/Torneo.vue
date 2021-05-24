96d4558bad0f422a963cd1be5980f32d5047c1e1
Santiago Jiménez <62775651+Santy172000@users.noreply.github.com>
2021-05-24 22:58:04 +0200

    Desactivar el botón de torneo si no se han puesto los participantes

--- src/views/Torneo.vue
+++ src/views/Torneo.vue
@@ -66,7 +66,14 @@
                       {{ $t('boton.reiniciar') }}
                     </button>
 
-                    <button type="button" class="btn btn-outline-secondary" @click="crearTorneo()">
+                    <button v-if="seleccionados" type="button" class="btn btn-outline-secondary" @click="crearTorneo()">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                      </svg>
+                      {{ $t('boton.crearTorneo') }}
+                    </button>
+
+                    <button v-if="!seleccionados" type="button" class="btn btn-outline-secondary disabled" @click="crearTorneo()">
                       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                       </svg>
@@ -201,7 +208,9 @@ export default {
   },
   computed: {
       ...mapState(['perfil','host','configuracion']), //Para recoger los datos de la lista de amigos que están almacenados en el store
-
+    seleccionados: function(){
+      return this.participantes[1].nombre != '' && this.participantes[2].nombre != '' && this.participantes[3].nombre != ''
+    }
   },
 
 }
